5899e03a41ec417118190f5a1d04f76babd6f674
Ivor Hung <ihung@rbbn.com>
2025-01-18 18:03:22 +0800

    Add build.sh

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ My build process for Intel ixgbe driver.
 https://sourceforge.net/projects/e1000/files/ixgbe%20stable/5.20.3/
 
 # How to 
-build.sh to complete all building to resolve compiler issues in Alam9.5 as well as RHEL9.5.
+
+'./build.sh all' to complete all building to resolve compiler issues in Alam9.5 as well as RHEL9.5.
+
+'./build.sh clean' to clean all obj and generated files.
--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-pushd ./ixgbe-5.20.3/src/
-make CFLAGS_MODULE="-Wno-error=incompatible-pointer-types" -j1
-popd
-cp ./ixgbe-5.20.3/src/ixgbe.ko ./
-xz -z ixgbe.ko
-ls -al ixgbe.ko.xz
-echo "################################"
-echo "# Done "
-echo " Copy ixgbe.ko.xz to E8300/E8100 /lib/modules/5.14.0-503.16.1.el9_5.x86_64/kernel/drivers/net/ethernet/intel/ixgbe"
-echo " And 'sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r)' to update initramfs"
-echo "################################"
+
+
+if [ "$#" -eq 0 ] || [ "$1" == "all" ]; then
+    echo "Building all..."
+    pushd ./ixgbe-5.20.3/src/
+    make CFLAGS_MODULE="-Wno-error=incompatible-pointer-types" -j1
+    popd
+    cp ./ixgbe-5.20.3/src/ixgbe.ko ./
+    xz -z ixgbe.ko
+    ls -al ixgbe.ko.xz
+    echo "################################"
+    echo "# Done "
+    echo " Copy ixgbe.ko.xz to /lib/modules/5.14.0-503.16.1.el9_5.x86_64/kernel/drivers/net/ethernet/intel/ixgbe"
+    echo " And 'sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r)' to update initramfs"
+    echo "################################"
+elif [ "$1" == "clean" ]; then
+    echo "Clean all..."
+    rm -f ./ixgbe-5.20.3/src/*.o
+    rm -f ./ixgbe-5.20.3/src/.*.cmd
+    rm -f ./ixgbe-5.20.3/src/Module.symvers
+    rm -f ./ixgbe-5.20.3/src/.ixgbe_main.o.d
+    rm -f ./ixgbe-5.20.3/src/ixgbe.ko
+    rm -f ./ixgbe-5.20.3/src/ixgbe.mod
+    rm -f ./ixgbe-5.20.3/src/ixgbe.mod.c
+    rm -f ./ixgbe-5.20.3/src/kcompat_generated_defs.h
+    rm -f ./ixgbe-5.20.3/src/modules.order
+    rm -f ./ixgbe.ko.xz
+else
+    echo "Invalid option. Usage: ./build.sh [all|clean]"
+fi
